a6246afffa25f1c970a5f04baf1d13fc7db6280c
João Vítor P. Santos <joaovitorps00@gmail.com>
2024-03-07 19:20:24 +0000

    add `.database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-const status = (request, response) => {
+import database from "../../../../infra/database.js";
+
+const status = async (request, response) => {
+  const result = await database.query("SELECT 1 + 1 as sum");
+  console.log(result.rows);
   response.status(200).json({ text: "Opa! é" });
 };
 
