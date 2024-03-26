22f37438cc06caaac089bf08a268b67935d1d31f
João Vítor P. Santos <joaovitorps00@gmail.com>
2024-03-26 00:50:26 +0000

    add `updated_at` to the `/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,7 @@
 import database from "/infra/database.js";
 
 const status = async (request, response) => {
-  const result = await database.query("SELECT 1 + 1 as sum");
-  console.log(result.rows);
-  response.status(200).json({ text: "Opa! é" });
+  response.status(200).json({ updated_at: new Date().toISOString() });
 };
 
 export default status;
