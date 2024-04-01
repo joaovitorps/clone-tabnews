43823ce6bbeba44bac934950d96a82bc3b6ff57b
João Vítor P. Santos <joaovitorps00@gmail.com>
2024-04-01 17:45:06 +0000

    add error handling for database connection and always close it in the end

--- infra/database.js
+++ infra/database.js
@@ -8,11 +8,17 @@ const query = async (queryObject) => {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
-  await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
 
-  return result;
+  await client.connect();
+
+  try {
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.log(error);
+  } finally {
+    await client.end();
+  }
 };
 
 export default {
