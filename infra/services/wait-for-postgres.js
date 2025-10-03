ce37183d00a40b373669eb9fd237325d4fdec2c2
Joao Santos <dev.joaosantos@outlook.com>
2025-10-03 10:29:31 -0300

    add `wait-for-postgres.js` script

---
+++ infra/services/wait-for-postgres.js
@@ -0,0 +1,17 @@
+const {exec} = require('node:child_process');
+
+const checkPostgresConnection = () => {
+    exec('docker exec postgres-dev pg_isready --host localhost', (err, stdout, stderr) => {
+        if (stdout.search('accepting connection') === -1) {
+            process.stdout.write('.');
+
+            checkPostgresConnection();
+            return;
+        }
+
+        console.log('\n✅ Conexão estabelecida.');
+    })
+}
+
+process.stdout.write('\n⚠️ Aguardando postgres estar pronto para aceitar conexões TCP/IP');
+checkPostgresConnection()
