a3fbe5a8b6966f6567a45119b5114ba91e1aead1
Joao Santos <dev.joaosantos@outlook.com>
2025-10-21 23:29:31 -0300

    test: add `describe` `Jest` function to describe tests better

--- tests/integration/api/v1/migrations/any.test.js
+++ tests/integration/api/v1/migrations/any.test.js
@@ -6,9 +6,13 @@ beforeAll(async () => {
   await database.query("drop schema public cascade; create schema public;");
 });
 
-test("Any HTTP method other than GET or POST should return 405 with a message", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "DELETE",
+describe("DELETE /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    test("Returning Not Allowed", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations", {
+        method: "DELETE",
+      });
+      expect(response.status).toBe(405);
+    });
   });
-  expect(response.status).toBe(405);
 });
--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -6,21 +6,18 @@ beforeAll(async () => {
   await database.query("drop schema public cascade; create schema public;");
 });
 
-test("GET to /api/v1/migrations should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations");
-  expect(response.status).toBe(200);
+describe("GET /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    test("Fetching run migrations", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations");
+      expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
+      const responseBody = await response.json();
 
-  expect(Array.isArray(responseBody)).toBe(true);
-  expect(responseBody.length).toBeGreaterThan(0);
-
-  const response2 = await fetch("http://localhost:3000/api/v1/migrations");
-  expect(response2.status).toBe(200);
-
-  const response2Body = await response2.json();
-  expect(Array.isArray(response2Body)).toBe(true);
-  expect(response2Body.length).toBeGreaterThan(0);
+      expect(Array.isArray(responseBody)).toBe(true);
+      expect(responseBody.length).toBeGreaterThan(0);
+    });
+  });
 });
 
 test("Jest should run on test environment", async () => {
